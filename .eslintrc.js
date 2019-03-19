0fab51f4b53165ec987e30e76558e43357756574
unknown <1216865281@qq.com>
2019-03-19 16:36:28 +0800

    indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -193,31 +193,33 @@
 module.exports = {
   root: true,
   env: {
-      node: true
+    node: true
   },
   'extends': [
-      'plugin:vue/essential',
-      '@vue/standard'
+    'plugin:vue/essential',
+    '@vue/standard'
   ],
   rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      // allow async-await
-      'generator-star-spacing': 'off',
-      // Default first line indentation 4
-      // 'vue/script-indent': ['error', 4, {'baseIndent': 1}],
-      'indent': ['error', 4, {'SwitchCase': 1}],
-      'object-curly-spacing': 0
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // Default first line indentation 4
+    // 'vue/script-indent': ['error', 4, {'baseIndent': 1}],
+    'indent': ['error', 4, {
+      'SwitchCase': 1
+    }],
+    'object-curly-spacing': 0
   },
-  'overrides': [// remove rslint indent rules
-      {
-          'files': ['*.vue'],
-          'rules': {
-              'indent': 'off'
-          }
+  'overrides': [ // remove rslint indent rules
+    {
+      'files': ['*.vue'],
+      'rules': {
+        'indent': 'off'
       }
+    }
   ],
   parserOptions: {
-      parser: 'babel-eslint'
+    parser: 'babel-eslint'
   }
 }
